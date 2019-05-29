37f661453e42b5ec7b0bedddf8e57e6effea2e89
asdadas <linerlander@gmail.com>
2019-05-29 10:49:55 -0500

    proyecto

---
+++ portalclubmascota.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="estilo.css">
+    <title>ClubMascota</title>
+</head>
+<body>
+    <div class="Portada">
+  <img id="Fondo-Portada" src="https://estaticos.elperiodico.com/resources/jpg/8/9/empresa-texana-viagen-pets-lider-eeuu-clonacion-mascotas-1519973966398.jpg" alt="">
+
+  <img id="Logo" src="Img/logo70px.png" alt="Logo">
+
+  <p id="Frase-Presentacion"> PORQUE EL MEJOR AMIGO DEL HOMBRE MERECE UNA VOZ</p>
+  <span class="icon-keyboard_arrow_down" id="Aprende-Mas"></span>
+</div>
+    <div>
+        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
+            <input type="hidden" name="cmd" value="_donations" />
+            <input type="hidden" name="business" value="anfurex.3351@gmail.com" />
+            <input type="hidden" name="currency_code" value="USD" />
+            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+            <img alt="" border="0" src="https://www.paypal.com/en_PE/i/scr/pixel.gif" width="1" height="1" />
+        </form>
+    </div>
+    <footer></footer>
+</body>
+</html>
