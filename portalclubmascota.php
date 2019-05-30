06cac7c5db7b93e6055efa27c454ce18b09948c8
manuel <anfurex.3351@gmail.com>
2019-05-30 02:47:49 -0500

    sdasdas}

--- portalclubmascota.php
+++ portalclubmascota.php
@@ -8,23 +8,18 @@
     <title>ClubMascota</title>
 </head>
 <body>
-    <div class="Portada">
-  <img id="Fondo-Portada" src="https://estaticos.elperiodico.com/resources/jpg/8/9/empresa-texana-viagen-pets-lider-eeuu-clonacion-mascotas-1519973966398.jpg" alt="">
-
-  <img id="Logo" src="Img/logo70px.png" alt="Logo">
-
-  <p id="Frase-Presentacion"> PORQUE EL MEJOR AMIGO DEL HOMBRE MERECE UNA VOZ</p>
-  <span class="icon-keyboard_arrow_down" id="Aprende-Mas"></span>
-</div>
-    <div>
-        <form action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-            <input type="hidden" name="cmd" value="_donations" />
-            <input type="hidden" name="business" value="anfurex.3351@gmail.com" />
-            <input type="hidden" name="currency_code" value="USD" />
-            <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
-            <img alt="" border="0" src="https://www.paypal.com/en_PE/i/scr/pixel.gif" width="1" height="1" />
-        </form>
+    <div class="banner">
+        <div class="menu">
+        <a href="">¿Que es Club Mascota?</a>
+        <a href="">Adopta</a>
+        <a href="">Únete</a>
+        </div>
     </div>
-    <footer></footer>
+    <div class="medio">
+    <p id="Frase-Presentacion"> PORQUE EL MEJOR AMIGO DEL HOMBRE MERECE UNA VOZ</p>
+    </div>
+    <div class="bajo">
+    <p id="Frase-Presentacion"> PORQUE EL MEJOR AMIGO DEL HOMBRE MERECE UNA VOZ</p>
+    </div>
 </body>
 </html>
