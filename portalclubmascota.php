012b872d7010312930d845b424fdc6ccf9075517
alumno informatica <alumno@ciis.usil.edu.pe>
2019-06-04 10:44:36 -0500

    masdmasdmas

--- portalclubmascota.php
+++ portalclubmascota.php
@@ -10,14 +10,33 @@
 <body>
     <div class="banner">
         <div class="menu">
-        <a href="">¿Que es Club Mascota?</a>
-        <a href="">Adopta</a>
-        <a href="">Únete</a>
+        <a href="http://www.wuf.pe/que-es">¿Que es Club Mascota?</a>
+        <a href="http://www.wuf.pe/adopta">Adopta</a>
+        <a href="http://www.wuf.pe/unete">Únete</a>
         </div>
     </div>
-    <div class="medio">
-    <p id="Frase-Presentacion"> PORQUE EL MEJOR AMIGO DEL HOMBRE MERECE UNA VOZ</p>
+    <div class="options-home">
+    <div class="options-content">
+        <a href="http://www.wuf.pe/adopta" class="options-box">
+            <div class="options-text">
+                <p class="adopta">ADOPTA</p>
+                <p>¿Buscas un perro? Elige un Wuf y cambia dos vidas!</p>
+            </div>
+        </a>
+        <a href="http://www.wuf.pe/unete" class="options-box">
+            <div class="options-text">
+                <p class="ayuda">AYUDA</p>
+                <p>¿Amas a los perros? Únete a la Manada WUF y sé parte del más grande programa de voluntariado canino.</p>
+            </div>
+        </a>
+                <a href="javascript:void(0)" class="options-box">
+            <div class="options-text">
+                <p class="contribuye">CONTRIBUYE</p>
+                                <p>Estamos rediseñando ClubWuf para generar mayor valor tanto a ti como para todos los wufs.</p>
+            </div>
+        </a>
     </div>
+</div>
     <div class="bajo">
     <p id="Frase-Presentacion"> PORQUE EL MEJOR AMIGO DEL HOMBRE MERECE UNA VOZ</p>
     </div>
