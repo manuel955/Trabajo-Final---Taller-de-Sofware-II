5076246555d154605554cfe5cdc7789de8c3ea1a
manuel955 <anfurex.3351@gmail.com>
2019-06-16 15:51:04 -0500

    sdadasd

---
+++ Ayuda.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/fontello.css">
+    <link rel="stylesheet" type="text/css" href="css/header.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/ayuda.css">
+    <title>Home Club Mascota</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor-header">
+        <h1><a class="icon-guidedog" href="home.php">Club Mascota</a></h1>
+            <div class="menu">
+                <ul>
+                    <li><a href="Que_Somos.php">¿Que es Club Mascota?</a></li>
+                    <li><a href="Adopta.php">Adopta</a></li>
+                    <li><a href="Ayuda.php">Ayuda</a></li>
+                    <li><a href="Contribuye.php">Contribuye</a></li>
+                    <li><a class="icon-user" href="http://www.wuf.pe/"></a></li>   
+                </ul>
+            </div>
+        </div>
+    </header>
+    <p>dsadasdasdasdasdasdasdasd</p>
+    <footer>sadasdasds</footer>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/fontello.css">
+    <link rel="stylesheet" type="text/css" href="css/header.css">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <link rel="stylesheet" type="text/css" href="css/home.css">
+    <title>Home Club Mascota</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor-header">
+        <h1><a class="icon-guidedog" href="home.php">Club Mascota</a></h1>
+            <div class="menu">
+                <ul>
+                    <li><a href="Que_Somos.php">¿Que es Club Mascota?</a></li>
+                    <li><a href="Adopta.php">Adopta</a></li>
+                    <li><a href="Ayuda.php">Ayuda</a></li>
+                    <li><a href="Contribuye.php">Contribuye</a></li>
+                    <li><a class="icon-user" href="http://www.wuf.pe/"></a></li> 
+                </ul>
+            </div>
+        </div>
+    </header>
+    <main>
+        <section id="banner">
+            <div class="contenedor-banner">
+                <h2>PORQUE EL MEJOR AMIGO DEL HOMBRE MERECE UNA VOZ</h2>
+            </div>
+            </div>
+            <a id="video-wuf-icon-modal" href="https://www.youtube.com/embed/W_haDz7I4I0?autoplay=1&color=white&fs=0&hl=es&showinfo=0&loop=1" target="_blank">
+                <i></i>
+                <span>Mira nuestro video</span>
+            </a>
+        </div>
+        </section>
+    </main>
+    </div>
+</body>
+</html>
